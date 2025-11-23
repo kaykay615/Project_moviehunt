6c1bb3fd22a61f0c32f9be90c063ee0e1bd4e5d1
Kaiptor3 <kaiptor18@gmail.com>
2025-11-23 12:21:18 -0300

    AGORA SIM A VERSÃO FINAL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.MovieHuntFinalVer.moviehunt',
   appName: 'MovieHunt',
   webDir: 'www'
 };
